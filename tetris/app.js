3f69908af82d6dbe3669187dbde0e388ee7f4879
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-27 16:32:01 -0500

    added freeze function, however it does not work

--- tetris/app.js
+++ tetris/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const width = 10 
 
 
-//  here are the tetrimonos
+	//  here are the tetrimonos
 	const lTetromino = [
 		[1, width+1, width*2+1, 2],
 		[width, width+1, width+2, width*2+2],
@@ -44,12 +44,15 @@ document.addEventListener('DOMContentLoaded', () => {
 		[width, width+1, width+2, width+3]
 	]
 
-	const theTetrominos = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
-
-	console.log(theTetrominos);
+	const theTetrominoes = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 	let currentPosition = 4
-	let current = theTetrominos[0][0]
+	let currentRotation = 0
+
+	console.log(theTetrominoes[0][0]);
+	// get a random tetromino 
+	let random = Math.floor(Math.random()*theTetrominoes.length)
+	let current = theTetrominoes[random][currentRotation]
 
 	// draw the tetrominos
 
@@ -59,7 +62,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	draw()
+
+	function undraw() {
+		current.forEach(index => {
+			squares[currentPosition + index].classList.remove('tetromino')
+		})
+	}
+	// make the tetromino move down every second 
+	timerId = setInterval(moveDown, 1000)
+
+	function moveDown () {
+		undraw()
+		currentPosition += width
+		draw()
+		freeze()
+	}
+
+	// write a freeze function 
+	function freeze() {
+		if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+			current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+			nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+			random = nextRandom
+			current = theTetrominoes[random][currentRotation]
+			currentPostition = 4
+			draw()
+		}
+	}
 
 })
 
